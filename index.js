31fd5e66ccb0952d06240d73d49d57e2d8ab6aba
jorge-alejandro21 <jorge.millan01@unicatolica.edu.co>
2024-10-29 00:17:58 -0500

    se actualizó el back con el cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { urlencoded, json } = require('express');
-const cors = require('cors');  // Ya tienes cors requerido aquí
+const cors = require('cors');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes.routes.js');
 const codesRoutes = require('./routes/codesRoutes.routes.js');
@@ -8,18 +8,20 @@ const port = process.env.PORT;
 
 const app = express();
 
+// Middleware
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
 // Configurar CORS para permitir solo solicitudes desde el dominio de tu frontend
 const corsOptions = {
-  origin: 'codigo-front-promo.vercel.app', // Reemplaza esto por la URL de tu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Métodos que vas a permitir
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Headers que permites
-  credentials: true  // Si estás manejando autenticación basada en cookies o sesiones
+  origin: 'https://codigo-front-promo.vercel.app', // Reemplaza esto por la URL de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos que vas a permitir
+  allowedHeaders: ['Content-Type', 'Authorization'], // Headers que permites
+  credentials: true // Si estás manejando autenticación basada en cookies o sesiones
 };
 
-app.use(cors(corsOptions));  // Usar las opciones de CORS
+// Usar CORS con las opciones configuradas
+app.use(cors(corsOptions));
 
 // Rutas de tu aplicación
 app.use('/user', userRoutes);
@@ -34,5 +36,5 @@ app.get('/user/login', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`listening at port http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
